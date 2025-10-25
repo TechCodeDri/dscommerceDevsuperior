0976b54ee87f95e6c745de06b91c7873a9854cfc
Drica2023 <adrianamedeiros.dev@outlook.com>
2025-10-25 13:57:19 -0300

    Dando continuidade ao projeto - Class Order

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/user.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/user.java
@@ -1,30 +1,37 @@
+
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class user {
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Long id;
-	String name;
-	String email;
-	String phone;
-	LocalDate birthDate;
-	String password;
-
-	public user() {
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
+	public User() {
 	}
 
-	public user(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -81,4 +88,9 @@ public class user {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	
 }
