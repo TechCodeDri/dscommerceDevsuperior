8a4b24c7d5b91d8c7f4d93973fda73b2f695ab81
Drica2023 <adrianamedeiros.dev@outlook.com>
2025-12-23 11:28:25 -0300

    Continuando o projeto - Payment

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscommerce.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,8 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_order")
@@ -19,13 +20,30 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
+
 	private OrderStatus status;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
+	public Order() {
+	}
+
+	public Order(long id, Instant moment, OrderStatus status, User client, Payment payment) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
+	}
 
 	public long getId() {
 		return id;
